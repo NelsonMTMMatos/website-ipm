1dc08ca652d431224c393031cab5631574737cd2
NelsonMTMMatos <nm.matos@campus.fct.unl.pt>
2024-10-01 00:19:38 +0100

    Added responsive navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
 
   return (
     <main>
-      <ul className="p-10 flex justify-between items-center flex-wrap lg:flex-nowrap">
-        {data.map((member, index) => (
-          <HeroCard key={index} person={member}/>
-        ))}
-      </ul>
+        <ul className="p-10 flex flex-col items-center justify-center gap-4 md:flex-row md:flex-wrap">
+          {data.map((member, index) => (
+            <HeroCard key={index} person={member}/>
+          ))}
+        </ul>
     </main>
   );
 }
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -2,7 +2,10 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import bars from '../../public/icons/bars.svg'
+import cross from '../../public/icons/cross.svg'
 
 const navLinks = [
     {
@@ -17,22 +20,37 @@ const navLinks = [
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [dropdownMenu, setDropdownMenu] = useState(false);
+  
+
+  const handleDropdownMenuClick = () => {
+    setDropdownMenu( prev => !prev)
+  }
 
   return (
-    <header className='flex justify-between items-center py-4 px-7 border-b bg-black'>
-    <Link href={'/'} className='text-white h-12 w-12 flex justify-center items-center bg-sky-500 hover:bg-sky-700'>Logo</Link>   
-    <nav className='text-white'>
-        <ul className='flex justify-center items-center gap-x-5'>
-        {
-            navLinks.map((link) => (
-                <li key={link.href} className={`hover:text-zinc-400 ${ pathname === link.href ? "font-bold" : ""}`}>
-                    <Link href={link.href}>
-                    {link.label}
-                    </Link>
-                </li>))
-        }
-        </ul>
-    </nav>
+    <header className=' bg-blue-950 text-white'>
+        <nav className=' px-4 py-10 h-16 flex items-center justify-between md:px-10'>
+            <Link href={'/'} className='h-12 w-12 flex items-center justify-center bg-sky-500 hover:bg-sky-700'>Logo</Link>
+            <div className={`absolute max-md:bg-blue-950  ${dropdownMenu ? 'top-20' : '-top-full'} left-0 w-full py-4 text-lg 
+                             flex flex-col md:static md:flex-row md:items-center md:justify-end md:visible`}>
+                <ul className=' flex flex-col items-center gap-6 md:flex-row'>
+                {
+                    navLinks.map((link) => (
+                        <li key={link.href} className={`hover:text-zinc-400 ${ pathname === link.href ? "font-bold" : ""}`}>
+                            <Link href={link.href}>
+                            {link.label}
+                            </Link>
+                        </li>))
+                }
+                </ul>
+            </div>
+            <div onClick={handleDropdownMenuClick} className='md:hidden'>
+                {
+                    dropdownMenu ? <Image src={cross} alt='' height={30} width={30}/> : 
+                                   <Image src={bars} alt='' height={30} width={30}/>
+                }
+            </div>
+        </nav>
     </header>
   )
 }
