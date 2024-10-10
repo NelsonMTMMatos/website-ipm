55ce00380c1022d34ecd9a394e1e069b46347153
NelsonMTMMatos <nm.matos@campus.fct.unl.pt>
2024-10-10 18:07:40 +0100

    Added Group Info to the initial page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ export default function Home() {
 
   return (
     <main>
+        <div className=" my-5 flex items-center justify-center">
+            <span className=" text-[60px]">Group 21</span>
+        </div>
         <ul className="p-10 flex flex-col items-center justify-center gap-4 md:flex-row md:flex-wrap">
           {data.map((member, index) => (
             <HeroCard key={index} person={member}/>
