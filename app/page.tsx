e615adbc0092f3d910dee95bfa3e9403e2cae79f
NelsonMTMMatos <nm.matos@campus.fct.unl.pt>
2024-10-27 23:59:50 +0000

    Disabled server-side rendering for Map, in order to fix Vercel Deployment.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import Map from "@/components/Map";
+import dynamic from 'next/dynamic';
+
+const Map = dynamic(() => import('@/components/Map'), { ssr: false });
 
 export default function Home() {
   return (
@@ -6,7 +8,7 @@ export default function Home() {
       <div className="flex-grow w-full max-w-3xl mx-auto">
         <Map />
       </div>
-      <div className=" pb-16"></div>
+      <div className="pb-16"></div>
     </main>
   );
 }
