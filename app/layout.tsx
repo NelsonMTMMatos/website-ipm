b65fac2592156aeeed6f69eef0a441280b781610
NelsonMTMMatos <nm.matos@campus.fct.unl.pt>
2024-11-20 18:20:53 +0000

    Trying to fix mobile problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
         <Container>
           <Navbar />
